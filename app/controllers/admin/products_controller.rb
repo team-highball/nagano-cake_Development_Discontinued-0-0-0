92dfa93253540eede76b55f3b8c0386028b0a501
vagrant <vagrant@localhost.localdomain>
2020-08-15 12:32:03 +0000

    making products

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -9,11 +9,16 @@ class Admin::ProductsController < ApplicationController
     end
 
     def new
-        
+        @product = Product.new
     end
 
     def create
-        
+        @product = Product.new(params[:id])
+        if @product.save
+            redirect_to admin_product_path(params[:id])
+        else
+            render "new"
+        end
     end
     
 
@@ -29,5 +34,10 @@ class Admin::ProductsController < ApplicationController
         
     end
     
-    
+    private
+
+    def product_params
+        params.require(:product).permit(:name, :product_image_id, :introduction, :genre_id, :price, :status)
+    end
+
 end
