cc59c5a87aef18fe57ed2e3e41d7c6bfca1a9f64
vagrant <hashibami2207@gmail.com>
2020-08-15 08:24:58 +0000

    [debug] app_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
 
   protected
 
-  def congfigure_permitted_parameters
+  def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :kana_fname, :kana_lname, :phone_number, :postal_code, :address])
   end
 end
